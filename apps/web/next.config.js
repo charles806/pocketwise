c9d585bc885b96b55a4bcfd87084939de1f08e88
charles806 <c08445333@gmail.com>
2026-05-23 10:26:17 +0100

    OMO

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,13 +1,24 @@
 const cspHeader = `
   default-src 'self';
+
   script-src 'self' 'unsafe-eval' 'unsafe-inline' https://va.vercel-scripts.com;
-  style-src 'self' 'unsafe-inline';
+
+  style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
+
+  font-src 'self' data: https://fonts.gstatic.com;
+
+  connect-src 'self' https://pocketwise.xyz;
+
   img-src 'self' blob: data: https://res.cloudinary.com;
-  font-src 'self' data:;
+
   object-src 'none';
+
   base-uri 'self';
+
   form-action 'self';
+
   frame-ancestors 'none';
+
   upgrade-insecure-requests;
 `;
 
